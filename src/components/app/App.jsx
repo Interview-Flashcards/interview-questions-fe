0dcc12a6b618cbfc23c0712f5d9474828aafe433
michmitz <stermitzm@gmail.com>
2021-01-27 15:19:14 -0800

    Structured components, added Flashcard component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import Banner from './Banner/Banner';
+import styles from '../../styles/styles.css';
+import QuestionItem from './Flashcard/Flashcard';
+import Flashcard from './Flashcard/Flashcard';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className={`${styles.wireframe} ${styles.appContainer}`}>
+      <Banner />
+      <Flashcard />
+    </div>
+    );
 }
---
+++ src/components/app/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../../styles/styles.css';
+
+export default function Banner() {
+  return (
+    <div className={`${styles.wireframe} ${styles.banner}`}>
+      <h1>Banner</h1>
+    </div>
+  )
+}
