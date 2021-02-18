5c9cd2b372e8427330da77a553f1cb6f5c135b92
Patrick Wilson <pmwilson333@gmail.com>
2021-02-18 14:54:26 -0800

    possible tag list generating

--- src/components/app/Banner/Banner.jsx
+++ src/components/app/Banner/Banner.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
+import { useFlashCards } from '../../../hooks/flashCards';
 import styles from '../../../styles/styles.css';
+import PropTypes from 'prop-types';
 
-export default function Banner() {
+export default function Banner({ chosenTag, onChange }) {
+  const { flashCards } = useFlashCards();
+  const possibleTagList = ['All'];
+
+
+
+  function isItemInArray(array, item) {
+    for(let i = 0; i < array.length; i++) {
+      if(item === array[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  for(let i = 0; i < flashCards.length; i++) {
+    const currentFlashCard = flashCards[i];
+
+    console.log('current flash card', currentFlashCard);
+
+    for(let j = 0; j < currentFlashCard.tags.length; j++) {
+      const currentTag = currentFlashCard.tags[j];
+
+      console.log('current tag', currentTag, isItemInArray(possibleTagList, currentTag));
+
+      if(currentTag !== '' && !isItemInArray(possibleTagList, currentTag)) {
+        possibleTagList.push(currentTag);
+      }
+    }
+  }
+
+  //maybe order array
+  console.log('possible', possibleTagList);
+
+  function handleTagChange(e) {
+    onChange(e.target.value);
+  }
+
   return (
     <div className={`${styles.wireframe} ${styles.banner}`}>
       <h1>Banner</h1>
+      <input value={chosenTag} onChange={handleTagChange} />
     </div>
-  )
+  );
 }
+
+Banner.propTypes = {
+  chosenTag: PropTypes.string,
+  onChange: PropTypes.func
+};
---
+++ src/components/containers/TagList.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Banner from '../app/Banner/Banner';
+import FlashCardList from '../app/FlashCardList/FlashCardList';
+
+export default function TagList() {
+  const [chosenTag, setChosenTag] = useState('All');
+
+  function handleTagChange(newValue) {
+    setChosenTag(newValue);
+    console.log('tag change', newValue);
+  }
+
+  return (
+    <>
+      <Banner chosenTag={chosenTag} onChange={handleTagChange}/>
+      <FlashCardList chosenTag={chosenTag}/>
+    </>
+  );
+}
