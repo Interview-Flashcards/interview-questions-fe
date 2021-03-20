00927a013a6922b16c84dee728983b64014680ee
michmitz <stermitzm@gmail.com>
2021-03-19 21:23:18 -0700

    Now rendering code challenge flashcards along with regular flashcards

---
+++ src/components/app/CodeChallengeFlashcard/CodeChallengeFlashcard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import flashcardStyles from '../Flashcard/flashcardStyles.css';
+import PropTypes from 'prop-types';
+import LinkItem from '../LinkItem/LinkItem'; 
+
+export default function CodeChallengeFlashcard({ id, question, answer, links }) {
+
+  return (
+    <div className={flashcardStyles.flashcard}>
+      <h2 className={flashcardStyles.question}>{question}</h2>
+      <h3>Level: {answer}</h3>
+      <LinkItem url={links[0].link} name={links[0].linkName}/>
+    </div>
+  );
+}
+
+CodeChallengeFlashcard.propTypes = {
+  id: PropTypes.string,
+  question: PropTypes.string,
+  answer: PropTypes.string,
+  hint: PropTypes.string,
+  category: PropTypes.string,
+  links: PropTypes.arrayOf(PropTypes.object)
+};
--- src/components/app/FlashCardList/FlashCardList.jsx
+++ src/components/app/FlashCardList/FlashCardList.jsx
@@ -3,12 +3,18 @@ import { useFlashCards } from '../../../hooks/flashCards';
 import Flashcard from '../Flashcard/Flashcard';
 import { isItemInArray } from '../../../api/bannerApi';
 import flashcardStyles from '../Flashcard/flashcardStyles.css';
+import CodeChallengeFlashcard from '../CodeChallengeFlashcard/CodeChallengeFlashcard';
 
 export default function FlashCardList({ chosenTag }) {
   const { flashCards } = useFlashCards();
 
   const allFlashCardElements = flashCards.filter(flashCard => flashCard.tags !== [])
     .map(flashCard => (
+      flashCard.category === 'code challenges' ?
+      <div key={flashCard.id}>
+        <CodeChallengeFlashcard {...flashCard} />
+      </div>
+      :
       <div key={flashCard.id}>
         <Flashcard {...flashCard} />
       </div>
@@ -16,6 +22,11 @@ export default function FlashCardList({ chosenTag }) {
 
   const flashCardElements = flashCards.filter(flashCard => isItemInArray(flashCard.tags, chosenTag))
     .map(flashCard => (
+      flashCard.category === 'code challenges' ?
+      <div key={flashCard.id}>
+        <CodeChallengeFlashcard {...flashCard} />
+      </div>
+      :
         <div key={flashCard.id}>
           <Flashcard {...flashCard} />
         </div>
