a72cf6e3e5b851d1aa9c202b2b2b4e6f3779999f
michmitz <stermitzm@gmail.com>
2021-04-26 19:26:02 -0700

    Added loading screen, scroll up button, moved root styles into main stylesheet

--- src/components/app/FlashCardList/FlashCardList.jsx
+++ src/components/app/FlashCardList/FlashCardList.jsx
@@ -3,10 +3,13 @@ import { useFlashCards } from '../../../hooks/flashCards';
 import Flashcard from '../Flashcard/Flashcard';
 import { isItemInArray } from '../../../api/bannerApi';
 import flashcardStyles from '../Flashcard/flashcardStyles.css';
+import styles from '../../../styles/styles.css';
 import CodeChallengeFlashcard from '../CodeChallengeFlashcard/CodeChallengeFlashcard';
 
 export default function FlashCardList({ chosenTag }) {
-  const { flashCards } = useFlashCards();
+  const { loading, flashCards } = useFlashCards();
+
+  if(loading) return <div className={styles.loadingDiv}><h1>Loading...</h1></div>;
 
   const allFlashCards = flashCards.filter(flashCard => flashCard.tags.length > 0)
     .map(flashCard => 
